2da816afd283b755c42ddba3a37c745212d95e74
Omar Sanchez <os2014425@gmail.com>
2024-03-26 22:19:59 -0500

    navbar añadido

--- wwwroot/js/navbar.js
+++ wwwroot/js/navbar.js
@@ -14,3 +14,30 @@ function activateIcon(icon) {
         setTimeout(() => icon.classList.remove('bounce'), 1000);
     }, 200);
 }
+
+// Función para redirigir al Home
+function redirectToHome() {
+    window.open('../Views/home.html', '_blank');
+}
+
+// Función para redirigir al Juego
+function redirectToGame() {
+    window.open('../Views/index.html', '_blank');
+}
+
+function loadChartPartialView() {
+    // Obtiene el contenido de ranking.html usando Fetch API o similar
+    fetch('../Views/ranking.html')
+      .then(response => response.text())
+      .then(data => {
+        // Actualiza el contenido del cuerpo modal con los datos obtenidos
+        document.getElementById('chartModal').querySelector('.modal-body').innerHTML = data;
+        // Muestra el modal
+        const chartModal = new bootstrap.Modal(document.getElementById('chartModal'));
+        chartModal.show();
+      })
+      .catch(error => {
+        console.error('Error al obtener ranking.html:', error);
+        // Maneja errores potenciales aquí (por ejemplo, muestra un mensaje de error)
+      });
+  }v
